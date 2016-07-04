a7cae0722db8d8db45cdac990cefa27513251ca2
Thomas Schraitle <tom_schr@web.de>
2016-07-04 21:56:27 +0200

    Create Python infrastructure

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import io
 import re
 from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
+from os.path import basename, dirname, join, splitext
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 def read(*names, **kwargs):
@@ -36,21 +31,22 @@ setup(
     url='https://github.com/tomschr/termquery',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    scripts=['src/scripts/termquery',
+             'src/scripts/termadmin'
+             ],
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
@@ -63,9 +59,11 @@ setup(
     ],
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
+        'terminology', 'database',
     ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'docopt',
     ],
     extras_require={
         # eg:
@@ -74,7 +72,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'termquery = termquery.cli:main',
+            'termadmin = termquery.cli:adminmain',
+            'termquery = termquery.cli:queryadmin'
         ]
     },
 )
